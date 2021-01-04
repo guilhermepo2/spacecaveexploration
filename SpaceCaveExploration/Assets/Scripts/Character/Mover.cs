89c8a524398f68bfe9809ec5578d7191bd35e6d9
Guilherme Oliveira <guilherme.061@gmail.com>
2021-01-04 16:20:58 -0500

    creating some tilemaps, animations, character controller, etc....

---
+++ SpaceCaveExploration/Assets/Scripts/Character/Mover.cs
@@ -0,0 +1,242 @@
+﻿#define DEBUG_RAYS
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+public class Mover : MonoBehaviour {
+    [System.Serializable]
+    protected struct RaycastOrigins {
+        public Vector3 TopLeft;
+        public Vector3 TopRight;
+        public Vector3 BottomRight;
+        public Vector3 BottomLeft;
+    }
+
+    [System.Serializable]
+    public class CollisionState {
+        public bool CollisionAbove;
+        public bool CollisionRight;
+        public bool CollisionBelow;
+        public bool CollisionLeft;
+        public bool BecameGroundedThisFrame;
+        public bool WasGroundedLastFrame;
+
+        public bool HasCollision() {
+            return (CollisionAbove || CollisionRight || CollisionBelow || CollisionLeft);
+        }
+
+        public void ResetCollision() {
+            CollisionAbove = CollisionRight = CollisionBelow = CollisionLeft = BecameGroundedThisFrame = false;
+        }
+    }
+
+    public event Action<RaycastHit2D> OnControllerCollidedEvent;
+    public event Action<Collider2D> OnTriggerEnterEvent;
+    public event Action<Collider2D> OnTriggerStayEvent;
+    public event Action<Collider2D> OnTriggerExitEvent;
+
+    protected bool IgnoreOneWayPlatformThisFrame;
+    private float m_SkinWidth = 0.0625f;
+    public float SkinWidth {
+        get {
+            return m_SkinWidth;
+        }
+        set {
+            m_SkinWidth = value;
+            RecalculateDistanceBetweenRays();
+        }
+    }
+
+    public LayerMask PlatformMask = 0;
+    public LayerMask TriggerMask = 0;
+    public LayerMask OneWayPlatformMask = 0;
+
+    private const int TOTAL_HORIZONTAL_RAYS = 6;
+    private const int TOTAL_VERTICAL_RAYS = 4;
+
+    private BoxCollider2D m_BoxCollider;
+    private Rigidbody2D m_Rigidbody;
+    protected CollisionState m_CollisionState = new CollisionState();
+    public bool IsGrounded { get { return m_CollisionState.CollisionBelow; } }
+    public Vector3 Velocity;
+    private const float SKIN_FUDGE_FACTOR = 0.001f;
+
+    protected RaycastOrigins m_RaycastOrigins;
+    private RaycastHit2D m_RaycastHit;
+    List<RaycastHit2D> m_RaycastHitsThisFrame = new List<RaycastHit2D>(2);
+
+    private float m_VerticalDistanceBetweenRays;
+    private float m_HorizontalDistanceBetweenRays;
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        OnTriggerEnterEvent?.Invoke(collision);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision) {
+        OnTriggerStayEvent?.Invoke(collision);
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        OnTriggerExitEvent?.Invoke(collision);
+    }
+
+    private void Awake() {
+        PlatformMask |= OneWayPlatformMask;
+        m_BoxCollider = GetComponent<BoxCollider2D>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+
+        SkinWidth = m_SkinWidth;
+        for(int i = 0; i < 32; i++) {
+            if((TriggerMask.value & 1 << 1) == 0) {
+                Physics2D.IgnoreLayerCollision(gameObject.layer, i);
+            }
+        }
+    }
+
+    private void OnValidate() {
+        Rigidbody2D tempRigidbody = GetComponent<Rigidbody2D>();
+
+        if(tempRigidbody != null) {
+            tempRigidbody.isKinematic = true;
+            tempRigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+            tempRigidbody.sleepMode = RigidbodySleepMode2D.NeverSleep;
+            tempRigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+        }
+    }
+
+    private void RecalculateDistanceBetweenRays() {
+        float ColliderUseableHeight = m_BoxCollider.size.y - (2f * m_SkinWidth);
+        float ColliderUseableWidth = m_BoxCollider.size.x - (2f * m_SkinWidth);
+
+        m_VerticalDistanceBetweenRays = ColliderUseableHeight / (TOTAL_HORIZONTAL_RAYS - 1);
+        m_HorizontalDistanceBetweenRays = ColliderUseableWidth / (TOTAL_VERTICAL_RAYS - 1);
+    }
+
+    private void CalculateRaycastOrigins() {
+        Bounds ModifiedBounds = m_BoxCollider.bounds;
+        ModifiedBounds.Expand(-2f * m_SkinWidth);
+
+        m_RaycastOrigins.TopRight = ModifiedBounds.max;
+        m_RaycastOrigins.TopLeft = new Vector2(ModifiedBounds.min.x, ModifiedBounds.max.y);
+        m_RaycastOrigins.BottomRight = new Vector2(ModifiedBounds.max.x, ModifiedBounds.min.y);
+        m_RaycastOrigins.BottomLeft = ModifiedBounds.min;
+    }
+
+    [System.Diagnostics.Conditional("DEBUG_RAYS")]
+    void DrawRay(Vector3 start, Vector3 dir, Color color) {
+        Debug.DrawRay(start, dir, color);
+    }
+
+    public void Move(Vector3 DeltaMovement) {
+        m_CollisionState.WasGroundedLastFrame = m_CollisionState.CollisionBelow;
+        m_CollisionState.ResetCollision();
+        m_RaycastHitsThisFrame.Clear();
+        CalculateRaycastOrigins();
+
+        if(DeltaMovement.x != 0f) {
+            MoveHorizontal(ref DeltaMovement);
+        }
+
+        if(DeltaMovement.y != 0f) {
+            MoveVertical(ref DeltaMovement);
+        }
+
+        DeltaMovement.z = 0f;
+        transform.Translate(DeltaMovement, Space.World);
+
+        if(Time.deltaTime > 0f) {
+            Velocity = DeltaMovement / Time.deltaTime;
+        }
+
+        if(!m_CollisionState.WasGroundedLastFrame && m_CollisionState.CollisionBelow) {
+            m_CollisionState.BecameGroundedThisFrame = true;
+        }
+
+        if(OnControllerCollidedEvent != null) {
+            for(int i = 0; i < m_RaycastHitsThisFrame.Count; i++) {
+                OnControllerCollidedEvent(m_RaycastHitsThisFrame[i]);
+            }
+        }
+
+        IgnoreOneWayPlatformThisFrame = false;
+    }
+
+    private void MoveHorizontal(ref Vector3 DeltaMovement) {
+        bool IsGoingRight = DeltaMovement.x > 0;
+        float RayDistance = Mathf.Abs(DeltaMovement.x) + m_SkinWidth;
+        Vector2 RayDirection = IsGoingRight ? Vector2.right : Vector2.left;
+        Vector2 InitialRayOrigin = IsGoingRight ? m_RaycastOrigins.BottomRight : m_RaycastOrigins.BottomLeft;
+
+        for(int i = 0; i < TOTAL_HORIZONTAL_RAYS; i++) {
+            Vector2 ray = new Vector2(InitialRayOrigin.x, InitialRayOrigin.y + i * m_VerticalDistanceBetweenRays);
+            DrawRay(ray, RayDirection * RayDistance, Color.red);
+
+            if( i == 0 && m_CollisionState.WasGroundedLastFrame) {
+                m_RaycastHit = Physics2D.Raycast(ray, RayDirection, RayDistance, PlatformMask);
+            } else {
+                m_RaycastHit = Physics2D.Raycast(ray, RayDirection, RayDistance, PlatformMask & ~OneWayPlatformMask);
+            }
+
+            if(m_RaycastHit) {
+                DeltaMovement.x = m_RaycastHit.point.x - ray.x;
+                RayDistance = Mathf.Abs(DeltaMovement.x);
+
+                if(IsGoingRight) {
+                    DeltaMovement.x -= m_SkinWidth;
+                    m_CollisionState.CollisionRight = true;
+                } else {
+                    DeltaMovement.x += m_SkinWidth;
+                    m_CollisionState.CollisionLeft = true;
+                }
+            }
+
+            m_RaycastHitsThisFrame.Add(m_RaycastHit);
+
+            if(RayDistance < m_SkinWidth + SKIN_FUDGE_FACTOR) {
+                break;
+            }
+        }
+    }
+
+    private void MoveVertical(ref Vector3 DeltaMovement) {
+        bool IsGoingUp = DeltaMovement.y > 0;
+        float RayDistance = Mathf.Abs(DeltaMovement.y) + m_SkinWidth;
+        Vector2 RayDirection = IsGoingUp ? Vector2.up : Vector2.down;
+        Vector2 InitialRayOrigin = IsGoingUp ? m_RaycastOrigins.TopLeft : m_RaycastOrigins.BottomLeft;
+
+        InitialRayOrigin.x += DeltaMovement.x;
+
+        var Mask = PlatformMask;
+        if(IsGoingUp && !m_CollisionState.WasGroundedLastFrame) {
+            Mask &= ~OneWayPlatformMask;
+        }
+
+        for(int i = 0; i < TOTAL_VERTICAL_RAYS; i++) {
+            var Ray = new Vector2(InitialRayOrigin.x + i * m_HorizontalDistanceBetweenRays, InitialRayOrigin.y);
+            DrawRay(Ray, RayDirection * RayDistance, Color.red);
+
+            m_RaycastHit = Physics2D.Raycast(Ray, RayDirection, RayDistance, Mask);
+
+            if(m_RaycastHit) {
+                DeltaMovement.y = m_RaycastHit.point.y - Ray.y;
+                RayDistance = Mathf.Abs(DeltaMovement.y);
+
+                if(IsGoingUp) {
+                    DeltaMovement.y -= m_SkinWidth;
+                    m_CollisionState.CollisionAbove = true;
+                } else {
+                    DeltaMovement.y += m_SkinWidth;
+                    m_CollisionState.CollisionBelow = true;
+                }
+
+                m_RaycastHitsThisFrame.Add(m_RaycastHit);
+
+                if(RayDistance < m_SkinWidth + SKIN_FUDGE_FACTOR) {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SpaceCaveExploration/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public enum EPlayerState {
+        EDummy,
+        ENormal,
+        EJumping
+    }
+
+    // ground velocity
+    [Header("Ground Velocity")]
+    public float RunSpeed = 3f;
+    public float GroundDamping = 5f;
+    public float InAirDamping = 2f;
+
+    [Header("Jumping")]
+    public float JumpPeakHeight = 2f;
+    public float HorizontalDistanceToJumpPeak = 2f;
+    public float JumpCutValue = 0.35f;
+    public float TerminalVelocity = -25.0f;
+    public float DownGravityMultiplier = 3f;
+    private const float JUMP_PRESSED_REMEMBER_TIME = 0.15f;
+    private const float GROUNDED_REMEMBER_TIME = 0.15f;
+
+    private float GoingUpGravity;
+    private float GoingDownGravity;
+    private float JumpInitialVelocity;
+
+    private float m_Gravity;
+    private float m_JumpPressedRemember;
+    private float m_GroundedRemember;
+    private Mover m_CharacterMover;
+
+    private float m_NormalizedHorizontalSpeed = 0.0f;
+    private EPlayerState m_CurrentPlayerState = EPlayerState.ENormal;
+
+    // ANIMATION NAMES
+    private string IdleAnimation = "Idle";
+    private string StandAnimation = "Stand";
+    private string RunAnimation = "Run";
+    private string JumpAnimation = "Jump";
+
+    private void Awake() {
+        GoingUpGravity = (-(2 * JumpPeakHeight * RunSpeed * RunSpeed)) / (HorizontalDistanceToJumpPeak * HorizontalDistanceToJumpPeak);
+        GoingDownGravity = GoingUpGravity * DownGravityMultiplier;
+        JumpInitialVelocity = ((2 * JumpPeakHeight * RunSpeed) / HorizontalDistanceToJumpPeak);
+        m_CharacterMover = GetComponent<Mover>();
+
+        m_CharacterMover.OnControllerCollidedEvent += OnControllerCollider;
+    }
+
+    private void Start() {
+        m_Gravity = GoingUpGravity;
+    }
+
+    void OnControllerCollider(RaycastHit2D hit) {
+        if (hit.normal.y == 1.0f) {
+            return;
+        }
+    }
+
+    private void Update() {
+        m_GroundedRemember -= Time.deltaTime;
+        m_JumpPressedRemember -= Time.deltaTime;
+        m_NormalizedHorizontalSpeed = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            m_JumpPressedRemember = JUMP_PRESSED_REMEMBER_TIME;
+        }
+
+        if(m_CharacterMover.IsGrounded) {
+            m_GroundedRemember = GROUNDED_REMEMBER_TIME;
+            m_CharacterMover.Velocity.y = 0.0f;
+        } else {
+            m_CurrentPlayerState = EPlayerState.EJumping;
+        }
+
+        switch(m_CurrentPlayerState) {
+            case EPlayerState.ENormal:
+                if(m_JumpPressedRemember > 0 && m_GroundedRemember > 0) {
+                    m_JumpPressedRemember = 0;
+                    m_GroundedRemember = 0;
+
+                    m_GroundedRemember = GoingUpGravity;
+                    m_CharacterMover.Velocity.y = JumpInitialVelocity;
+                    m_CurrentPlayerState = EPlayerState.EJumping;
+                }
+                break;
+            case EPlayerState.EJumping:
+                if(Input.GetKeyUp(KeyCode.Space) && m_CharacterMover.Velocity.y > 0) {
+                    m_CharacterMover.Velocity.y *= JumpCutValue;
+                }
+
+                if(m_CharacterMover.IsGrounded) {
+                    m_CurrentPlayerState = EPlayerState.ENormal;
+                }
+                break;
+        }
+
+        ProcessSpriteScale();
+        ProcessAnimation();
+
+        if(m_CharacterMover.Velocity.y < 0.0f) {
+            m_Gravity = GoingDownGravity;
+            m_CurrentPlayerState = EPlayerState.EJumping;
+        }
+
+        float SmoothedMovementFactor = m_CharacterMover.IsGrounded ? GroundDamping : InAirDamping;
+
+        // force removing momentum when player is not pressing any key
+        if(m_NormalizedHorizontalSpeed == 0) {
+            SmoothedMovementFactor *= 2.5f;
+        }
+
+        m_CharacterMover.Velocity.x = Mathf.Lerp(m_CharacterMover.Velocity.x, m_NormalizedHorizontalSpeed * RunSpeed, SmoothedMovementFactor * Time.deltaTime);
+        m_CharacterMover.Velocity.y = Mathf.Max(TerminalVelocity, (m_CharacterMover.Velocity.y + (m_Gravity * Time.deltaTime)));
+
+        Vector2 VerletVelocity = new Vector2(m_CharacterMover.Velocity.x, m_CharacterMover.Velocity.y + (0.5f * m_Gravity * Time.deltaTime * Time.deltaTime));
+        Vector2 VerletDeltaMovement = VerletVelocity * Time.deltaTime;
+
+        m_CharacterMover.Move(VerletDeltaMovement);
+    }
+
+    private void ProcessSpriteScale() {
+        if(m_NormalizedHorizontalSpeed == 0 && Mathf.Abs(m_CharacterMover.Velocity.x) < Mathf.Epsilon) {
+            return;
+        }
+
+        float signal =  Mathf.Abs(m_NormalizedHorizontalSpeed) > Mathf.Abs(m_CharacterMover.Velocity.x) ?
+            Mathf.Sign(m_NormalizedHorizontalSpeed) :
+            Mathf.Sign(m_CharacterMover.Velocity.x);
+
+        transform.localScale = new Vector3(signal * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    private void ProcessAnimation() {
+        // cache this reference when the game begins
+        Animator m_Animator = GetComponentInChildren<Animator>();
+
+        if(Mathf.Abs(m_CharacterMover.Velocity.y) > Mathf.Epsilon) {
+            m_Animator.Play(JumpAnimation);
+        } else if(Mathf.Abs(m_NormalizedHorizontalSpeed) > 0.1f) {
+            m_Animator.Play(RunAnimation);
+        } else {
+            m_Animator.Play(StandAnimation);
+        }
+    }
+
+}
